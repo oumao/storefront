84c5b592f188428a5a992115fde87e762ab19b4b
oumao <evansotis2015@gmail.com>
2023-01-11 00:11:11 +0300

    Finished writing CRUD for users

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,67 +1,124 @@
 import express, { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 import {User, UserModel } from '../models/users.model'
+
+dotenv.config()
 
 // Instantiating user model
 const userStore = new UserModel()
 
+// Environment variables
+const {
+    TOKEN_SECRET
+} = process.env
 
 // Controller to create user
 const registerUser = async (_req: Request, res: Response) => {
     try {
 
-        const user =  {
+        const userObject =  {
             username: _req.body.username,
             firstName: _req.body.firstName,
-            lastName: _req.body.las,
+            lastName: _req.body.lastName,
             password_digest: _req.body.password_digest
         }
 
-        const result = await userStore.createUser(user)
+        const userExists = await userStore.getUserByUsername(userObject.username)
 
-        
+        if(userExists){
+            res.status(400).json(`User with username ${userObject.username} already exists!!`)
+        } else{
+            const user = await userStore.createUser(userObject)
 
+            const token = jwt.sign({ id: user.id, username: user.username}, TOKEN_SECRET as string)
+            
+            res.status(201).json(token)
+        }
     } catch (error) {
-        res.status(400).json("Couldnot Create User")
+        res.json(error)
     }
 }
 
 // Controller to create user
 const getUsers = async (_req: Request, res: Response) => {
     try {
-
+        const users = await userStore.getAllUsers()
+        res.status(200).json(users)
         
     } catch (error) {
-        res.status(400).json("Couldnot Create User")
+        res.json(error)
     }
 }
 
 
 // Controller to create user
 const getUser = async (_req: Request, res: Response) => {
+    const userId = _req.params.id as unknown as number
     try {
+        const user = await userStore.getUserById(userId)
 
-        
+        if(!user) {
+            res.status(404).json(`No user with id ${userId} found!`)
+        }
+        res.status(200).json(user)
     } catch (error) {
-        res.status(400).json("Couldnot Create User")
+        res.status(400).json(error)
     }
 }
 
 // Controller to create user
 const editUser = async (_req: Request, res: Response) => {
+    const userId = _req.params.id as unknown as number
     try {
+        const user = await userStore.getUserById(userId)
 
+        // check if user doesnt exist
+        if(!user){
+            res.status(404).json(`User with id ${userId} doesnot exist!`)
+        }else{
+
+            const userObject = {
+                id: parseInt(_req.params.id as string),
+                username: _req.body.username as string,
+                firstName: _req.body.firstName as string,
+                lastName: _req.body.lastName as string,
+                password_digest: _req.body.password_digest as string
+            }
+
+            // update user
+            const result = await userStore.updateUser(userObject)
+    
+            res.status(200).json(result.username)
+        }
         
     } catch (error) {
-        res.status(400).json("Couldnot Create User")
+        res.status(400).json(error)
     }
 }
 
 // Controller to create user
 const removeUser = async (_req: Request, res: Response) => {
+    const userId = _req.params.id as unknown as number 
     try {
+        const user = await userStore.getUserById(userId)
 
-        
+        // check if user doesnt exist
+        if(!user){
+            res.status(404).json(`User with id ${userId} doesnot exist!`)
+        }else{
+
+            // delete user from the database
+            await userStore.deleteUser(userId)
+    
+            res.send(200).json(`User successfully deleted`)
+        }
+
     } catch (error) {
-        res.status(400).json("Couldnot Create User")
+        res.status(400).json("Couldnot delete User ")
     }
 }
+
+
+
+export { registerUser, getUsers, getUser, editUser, removeUser }
