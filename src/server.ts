62c05eba3d956487dfa132994141c18d058c6835
oumao <evansotis2015@gmail.com>
2023-01-11 00:12:54 +0300

    Added user routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import userRoutes from './routes/users.route'
 
 const app: express.Application = express();
 const address = '0.0.0.0:3000' as string;
 
 app.use(bodyParser.json());
 
-app.get('/', function (req: Request, res: Response) {
-  res.send('Hello World!');
-});
+// Routes
+app.use("/", userRoutes)
 
 app.listen(3000, function () {
   console.log(`starting app on: ${address}`);
