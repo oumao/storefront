a5494bb40fa91ccd38da743020417bc68f0986d6
oumao <evansotis2015@gmail.com>
2023-01-11 22:50:46 +0300

    Completed CRUD functionality for Orders

---
+++ src/controllers/orders.controller.ts
@@ -0,0 +1,100 @@
+import { Order, OrderModel } from "../models/orders.model"
+import { Request, Response } from "express"
+
+
+const orderStore = new OrderModel()
+
+const getOrders = async (_req: Request, res: Response) => {
+    try {
+        const orders = await orderStore.getAllOrders()
+
+        res.status(200).json(orders)
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+
+const getOrder = async (_req: Request, res: Response) => {
+    try {
+        const order = await orderStore.getSingleOrder(_req.params.id as unknown as number)
+
+        if(!order){
+            res.status(404).json(`The order doesnt exist!`)
+        }else{
+            res.status(200).json(order)
+        }
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+
+const createOrder = async (_req: Request, res: Response) => {
+    try {
+
+
+        const orderObject = {
+            user_id: _req.body.user_id,
+            status: _req.body.status
+        }
+
+        const orders = await orderStore.create(orderObject)
+
+        res.status(200).json(orders)
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+
+const updateOrder = async (_req: Request, res: Response) => {
+    try {
+
+        const order = await orderStore.getSingleOrder(_req.params.id as unknown as number)
+
+        if(!order){
+            res.status(404).json(`The order doesnt exist!`)
+        }else{
+            const orderObject = {
+                id: order.id,
+                user_id: _req.body.user_id,
+                status: _req.body.status
+            }
+            const result = await orderStore.updateSingleOrder(orderObject)
+            res.status(200).json(result)
+        }
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+
+const deleteOrder = async (_req: Request, res: Response) => {
+    try {
+
+        const order = await orderStore.getSingleOrder(_req.params.id as unknown as number)
+
+        if(!order){
+            res.status(404).json(`The order doesnt exist!`)
+        }else{
+            const result = await orderStore.deleteSingleOrder(_req.params.id as unknown as number)
+            res.status(200).json(`Order with id ${_req.params.id} has been deleted!`)
+        }
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+export {
+    createOrder,
+    getOrders,
+    getOrder,
+    updateOrder,
+    deleteOrder
+}
