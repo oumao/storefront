8a4490e05e7e0d0ca991e3ae226d7934865e40d7
oumao <evansotis2015@gmail.com>
2023-01-12 23:43:57 +0300

    Added functionality to register product on order

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -50,6 +50,26 @@ const createOrder = async (_req: Request, res: Response) => {
     }
 }
 
+const createProductOrder = async (_req: Request, res: Response) => {
+    try {
+        const orderId = _req.params.orderId as unknown as number
+        const productId = _req.params.productId as unknown as number
+        const qty = _req.body.quantity
+        
+        const orderProductObject = {
+            quantity: qty,
+            order_id: orderId,
+            product_id: productId
+        }
+
+        const result = await orderStore.createOrderWithProducts(orderProductObject)
+        res.status(200).json(result)
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
 
 const updateOrder = async (_req: Request, res: Response) => {
     try {
@@ -93,6 +113,7 @@ const deleteOrder = async (_req: Request, res: Response) => {
 
 export {
     createOrder,
+    createProductOrder,
     getOrders,
     getOrder,
     updateOrder,
