233db07dd5d4f02b4d319324e58a8bc098074675
oumao <evansotis2015@gmail.com>
2023-01-12 23:45:13 +0300

    Update orders routes for OrderProduct

--- src/routes/orders.route.ts
+++ src/routes/orders.route.ts
@@ -1,11 +1,12 @@
 import express, { Router } from "express"
-import { createOrder, getOrders, getOrder, updateOrder, deleteOrder } from "../controllers/orders.controller"
+import { createOrder, createProductOrder, getOrders, getOrder, updateOrder, deleteOrder } from "../controllers/orders.controller"
 
 const orderRoutes: Router = express.Router()
 
 
 orderRoutes.route("/orders").get(getOrders).post(createOrder)
 orderRoutes.route("/orders/:id").get(getOrder).put(updateOrder).delete(deleteOrder)
+orderRoutes.route("/orders/:orderId/products/:productId").post(createProductOrder)
 
 
 export default orderRoutes
