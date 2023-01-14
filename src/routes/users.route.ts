2af8a1092a51a848ebc274582c3678e3d9197670
oumao <evansotis2015@gmail.com>
2023-01-14 23:36:27 +0300

    Updated checkout route

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,15 @@
 import express, { Router } from 'express'
-import  { registerUser, getUsers, getUser, editUser, removeUser } from '../controllers/users.controller'
+import {
+  registerUser,
+  getUsers,
+  getUser,
+  editUser,
+  removeUser,
+} from '../controllers/users.controller'
 
 const userRoutes: Router = express.Router()
 
-
-userRoutes.route("/users").get(getUsers).post(registerUser)
-userRoutes.route("/users/:id").get(getUser).put(editUser).delete(removeUser)
+userRoutes.route('/users').get(getUsers).post(registerUser)
+userRoutes.route('/users/:id').get(getUser).put(editUser).delete(removeUser)
 
 export default userRoutes
