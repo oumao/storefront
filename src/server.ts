d9299e6cced468ce51ae243b363d035a85c84d3f
oumao <evansotis2015@gmail.com>
2023-01-14 23:37:00 +0300

    Prettier checked

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
 import apiRoutes from './routes/index.route'
 
-const app: express.Application = express();
-const address = '0.0.0.0:3000' as string;
+const app: express.Application = express()
+const address = '0.0.0.0:3000' as string
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // Routes
-app.use("/", apiRoutes)
+app.use('/', apiRoutes)
 
 app.listen(3000, function () {
-  console.log(`starting app on: ${address}`);
-});
+  console.log(`starting app on: ${address}`)
+})
