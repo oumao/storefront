ca2fc81c4df49ab0b018f71bca9a9b717be8cb3b
oumao <evansotis2015@gmail.com>
2023-01-14 23:38:22 +0300

    Refactor the models

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -1,116 +1,116 @@
-import client from "../database/database"
-import { OrderProduct } from "../types/order.products.types"
+import client from '../database/database'
+import { OrderProduct } from '../types/order.products.types'
 
 export type Order = {
-    id?: number,
-    status: string,
-    user_id: number
+  id?: number
+  status: string
+  user_id: number
 }
 
-
 export class OrderModel {
+  // Create an Order
+  async create(o: Order): Promise<Order[]> {
+    try {
+      const conn = await client.connect()
 
-    // Create an Order
-    async create(o: Order): Promise<Order> {
-        try {
-            const conn = await client.connect()
+      const sql = `INSERT INTO orders (user_id, status) VALUES($1, $2) RETURNING *`
 
-            const sql = `INSERT INTO orders (user_id, status) VALUES($1, $2) RETURNING *`
+      const result = await conn.query(sql, [o.user_id, o.status])
 
-            const result = await conn.query(sql, [o.user_id, o.status])
+      conn.release()
 
-            conn.release()
-
-            return result.rows
-        } catch (error) {
-            throw new Error(`Couldnot create order. Error ${error}`)
-        }
+      return result.rows
+    } catch (error) {
+      throw new Error(`Couldnot create order. Error ${error}`)
     }
+  }
 
-    // Create an Order with Products
-    async createOrderWithProducts(po: OrderProduct): Promise<OrderProduct> {
-        try {
-            const conn = await client.connect()
+  // Create an Order with Products
+  async createOrderWithProducts(po: OrderProduct): Promise<OrderProduct> {
+    try {
+      const conn = await client.connect()
 
-            const sql = `INSERT INTO order_products (quantity, order_id, product_id) VALUES($1, $2, $3) RETURNING *`
+      const sql = `INSERT INTO order_products (quantity, order_id, product_id) VALUES($1, $2, $3) RETURNING *`
 
-            const result = await conn.query(sql, [po.quantity, po.order_id, po.product_id])
+      const result = await conn.query(sql, [
+        po.quantity,
+        po.order_id,
+        po.product_id,
+      ])
 
-            conn.release()
+      conn.release()
 
-            return result.rows[0]
-        } catch (error) {
-            throw new Error(`Couldnot create products in an order. Error ${error}`)
-        }
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Couldnot create products in an order. Error ${error}`)
     }
+  }
 
-    // Get list of Orders
-    async getAllOrders(): Promise<Order []> {
-        try {
-            const conn = await client.connect()
+  // Get list of Orders
+  async getAllOrders(): Promise<Order[]> {
+    try {
+      const conn = await client.connect()
 
-            const sql = `SELECT * FROM orders`
+      const sql = `SELECT * FROM orders`
 
-            const result = await conn.query(sql)
+      const result = await conn.query(sql)
 
-            conn.release()
+      conn.release()
 
-            return result.rows
-        } catch (error) {
-            throw new Error(`Couldnot retrieve orders. Error ${error}`)
-        }
+      return result.rows
+    } catch (error) {
+      throw new Error(`Couldnot retrieve orders. Error ${error}`)
     }
+  }
 
-    // Get Single specific Order
-    async getSingleOrder(id: number): Promise<Order> {
-        try {
-            const conn = await client.connect()
+  // Get Single specific Order
+  async getSingleOrder(id: number): Promise<Order> {
+    try {
+      const conn = await client.connect()
 
-            const sql = `SELECT * FROM orders WHERE id=$1`
+      const sql = `SELECT * FROM orders WHERE id=$1`
 
-            const result = await conn.query(sql, [id])
+      const result = await conn.query(sql, [id])
 
-            conn.release()
+      conn.release()
 
-            return result.rows[0]
-        } catch (error) {
-            throw new Error(`Couldnot retrieve the order ${id}. Error ${error}`)
-        }
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Couldnot retrieve the order ${id}. Error ${error}`)
     }
+  }
 
-    // Update Order Method
-    async updateSingleOrder(o: Order): Promise<Order> {
-        try {
-            const conn = await client.connect()
+  // Update Order Method
+  async updateSingleOrder(o: Order): Promise<Order> {
+    try {
+      const conn = await client.connect()
 
-            const sql = `UPDATE orders SET user_id=$2, status=$3 WHERE id=$1 RETURNING *`
+      const sql = `UPDATE orders SET user_id=$2, status=$3 WHERE id=$1 RETURNING *`
 
-            const result = await conn.query(sql, [o.id, o.user_id, o.status])
+      const result = await conn.query(sql, [o.id, o.user_id, o.status])
 
-            conn.release()
+      conn.release()
 
-            return result.rows[0]
-        } catch (error) {
-            throw new Error(`Couldnot update the order. Error ${error}`)
-        }
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Couldnot update the order. Error ${error}`)
     }
+  }
 
-    // Delete Order method
-    async deleteSingleOrder(id: number): Promise<Order> {
-        try {
-            const conn = await client.connect()
+  // Delete Order method
+  async deleteSingleOrder(id: number): Promise<Order> {
+    try {
+      const conn = await client.connect()
 
-            const sql = `DELETE FROM orders WHERE id=$1`
+      const sql = `DELETE FROM orders WHERE id=$1`
 
-            const result = await conn.query(sql, [id])
+      const result = await conn.query(sql, [id])
 
-            conn.release()
+      conn.release()
 
-            return result.rows[0]
-        } catch (error) {
-            throw new Error(`Couldnot delete the order ${id}. Error ${error}`)
-        }
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Couldnot delete the order ${id}. Error ${error}`)
     }
-
-    
+  }
 }
--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,119 +1,125 @@
-import client from "../database/database"
+import client from '../database/database'
 import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-const {
-    SALT_ROUNDS,
-    BCRYPT_SECRET
-} = process.env
+const { SALT_ROUNDS, BCRYPT_SECRET } = process.env
 
 export type User = {
-    id?: number,
-    username: string,
-    firstName: string, 
-    lastName: string,
-    password_digest: string
+  id?: number
+  username: string
+  firstName: string
+  lastName: string
+  password_digest: string
 }
 
 export class UserModel {
+  async createUser(u: User): Promise<User> {
+    try {
+      const conn = await client.connect()
+      const sql = `INSERT INTO users (username, firstName, lastName, password_digest) VALUES($1, $2, $3, $4) RETURNING *`
 
-    async createUser(u: User): Promise<User> {
-        try {
-            const conn = await client.connect()
-            const sql = `INSERT INTO users (username, firstName, lastName, password_digest) VALUES($1, $2, $3, $4) RETURNING *`
+      // Generating hashed password using bcrypt
+      u.password_digest = bcrypt.hashSync(
+        u.password_digest + BCRYPT_SECRET,
+        parseInt(SALT_ROUNDS as string)
+      )
 
-            // Generating hashed password using bcrypt
-            u.password_digest = bcrypt.hashSync(u.password_digest + BCRYPT_SECRET, parseInt(SALT_ROUNDS as string))
+      const result = await conn.query(sql, [
+        u.username,
+        u.firstName,
+        u.lastName,
+        u.password_digest,
+      ])
 
-            const result = await conn.query(sql, [u.username, u.firstName, u.lastName, u.password_digest])
+      conn.release()
 
-            conn.release()
-
-            return result.rows[0]
-
-        } catch (error) {
-           throw new Error(`Couldnot create user ${u.username}. Error ${error}`) 
-        }
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Couldnot create user ${u.username}. Error ${error}`)
     }
+  }
 
-    async getUserById(id: number): Promise<User> {
-        try {
-            const conn = await client.connect()
-            const sql = `SELECT * FROM users WHERE id=$1`
+  async getUserById(id: number): Promise<User> {
+    try {
+      const conn = await client.connect()
+      const sql = `SELECT * FROM users WHERE id=$1`
 
-            const result = await conn.query(sql, [id])
+      const result = await conn.query(sql, [id])
 
-            conn.release()
+      conn.release()
 
-            return result.rows[0]
-        } catch (error) {
-           throw new Error(`Couldnot retrieve user ${id}. Error ${error}`) 
-        }
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Couldnot retrieve user ${id}. Error ${error}`)
     }
+  }
 
-    async getUserByUsername(username: string): Promise<User> {
-        try {
-            const conn = await client.connect()
-            const sql = `SELECT * FROM users WHERE username=$1`
+  async getUserByUsername(username: string): Promise<User> {
+    try {
+      const conn = await client.connect()
+      const sql = `SELECT * FROM users WHERE username=$1`
 
-            const result = await conn.query(sql, [username])
+      const result = await conn.query(sql, [username])
 
-            conn.release()
+      conn.release()
 
-            return result.rows[0]
-        } catch (error) {
-           throw new Error(`Couldnot retrieve user ${username}. Error ${error}`) 
-        }
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Couldnot retrieve user ${username}. Error ${error}`)
     }
+  }
 
-    async getAllUsers(): Promise<User []> {
-        try {
-            const conn = await client.connect()
-            const sql = `SELECT username, firstName, lastName FROM users`
+  async getAllUsers(): Promise<User[]> {
+    try {
+      const conn = await client.connect()
+      const sql = `SELECT id, username, firstName, lastName FROM users`
 
-            const result = await conn.query(sql)
+      const result = await conn.query(sql)
 
-            conn.release()
+      conn.release()
 
-            return result.rows
-
-        } catch (error) {
-           throw new Error(`Couldnot retrieve user list. Error ${error}`) 
-        }
+      return result.rows
+    } catch (error) {
+      throw new Error(`Couldnot retrieve user list. Error ${error}`)
     }
+  }
 
-    async updateUser(u: User): Promise<User> {
-        try {
-            const conn = await client.connect()
-            const sql = `UPDATE users SET username=$2, firstName=$3, lastName=$4, password_digest=$5 WHERE id=$1 RETURNING *`
-            u.password_digest = bcrypt.hashSync(u.password_digest+BCRYPT_SECRET, parseInt(SALT_ROUNDS as string))
-            const result = await conn.query(sql, [u.id, u.username, u.firstName, u.lastName, u.password_digest])
-            conn.release()
+  async updateUser(u: User): Promise<User> {
+    try {
+      const conn = await client.connect()
+      const sql = `UPDATE users SET username=$2, firstName=$3, lastName=$4, password_digest=$5 WHERE id=$1 RETURNING *`
+      u.password_digest = bcrypt.hashSync(
+        u.password_digest + BCRYPT_SECRET,
+        parseInt(SALT_ROUNDS as string)
+      )
+      const result = await conn.query(sql, [
+        u.id,
+        u.username,
+        u.firstName,
+        u.lastName,
+        u.password_digest,
+      ])
+      conn.release()
 
-            return result.rows[0]
-        } catch (error) {
-           throw new Error(`Couldnot update user ${u.id}. Error ${error}`) 
-        }
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Couldnot update user ${u.id}. Error ${error}`)
     }
+  }
 
-    
+  async deleteUser(id: number): Promise<User> {
+    try {
+      const conn = await client.connect()
+      const sql = `DELETE FROM users WHERE id=$1`
 
-    async deleteUser(id: number): Promise<User> {
-        try {
-            const conn = await client.connect()
-            const sql = `DELETE FROM users WHERE id=$1`
+      const result = await conn.query(sql, [id])
+      conn.release()
 
-            const result = await conn.query(sql, [id])
-            conn.release()
-
-            return result.rows[0]
-        } catch (error) {
-           throw new Error(`Couldnot delete user ${id}. Error ${error}`) 
-        }
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Couldnot delete user ${id}. Error ${error}`)
     }
-
-
-
+  }
 }
