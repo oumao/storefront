8f57abfe00bc154d1331fbc4f5ced091e726759f
oumao <evansotis2015@gmail.com>
2023-01-14 23:37:59 +0300

    Implemented CRUD for products, users and orders

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -1,59 +1,82 @@
-// import { DashboardQueries } from "../models/dashboard.service"
-// import { Request, Response } from "express"
+import { DashboardQueries } from '../models/dashboard.service'
+import { Request, Response } from 'express'
+import { OrderModel } from '../models/orders.model'
 
-// const dashboard = new DashboardQueries()
+const dashboard = new DashboardQueries()
+const orderStore = new OrderModel()
 
-// export const getUserOrder =async (_req: Request, res: Response) => {
-//     try {
-//         const userId = _req.params.user_id
-//         const result = await dashboard.getUserWithOrder(_req.params.user_id)
-        
-//         if(!result){
-//             res.status(404).json(`No user with id ${userId}`)
-//         }
-//         res.status(200).json(result)
-//     } catch (error) {
-        
-//     }
-// }
+export const getUserOrder = async (_req: Request, res: Response) => {
+  try {
+    const userId = _req.params.user_id
+    const result = await dashboard.getUserWithOrder(userId)
 
+    if (!result) {
+      res.status(404).json(`No user with id ${userId}`)
+    }
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
 
-// export const getUsersOrder =async (_req: Request, res: Response) => {
-//     try {
-//         const result = await dashboard.getUsersWithOrders()
-//         res.status(200).json(result)
-//     } catch (error) {
-        
-//     }
-// }
+export const getUsersOrders = async (_req: Request, res: Response) => {
+  try {
+    const result = await dashboard.getUsersWithOrders()
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
 
-// export const getProductsOrders =async (_req: Request, res: Response) => {
-//     try {
-//         const userId = _req.params.user_id
-//         const result = await dashboard.getProductsInOrders()
-        
-//         if(!result){
-//             res.status(404).json(`No user with id ${userId}`)
-//         }
-//         res.status(200).json(result)
-//     } catch (error) {
-        
-//     }
-// }
+export const getProductsOrders = async (_req: Request, res: Response) => {
+  try {
+    const result = await dashboard.getProductsInOrders()
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
 
-// export const getUserOrder =async (_req: Request, res: Response) => {
-//     try {
-//         const userId = _req.params.user_id
-//         const result = await dashboard.getUserWithOrder(_req.params.user_id)
-        
-//         if(!result){
-//             res.status(404).json(`No user with id ${userId}`)
-//         }
-//         res.status(200).json(result)
-//     } catch (error) {
-        
-//     }
-// }
+export const getProductsOrder = async (_req: Request, res: Response) => {
+  try {
+    const orderId = _req.params.orderId as unknown as number
 
+    const result = await dashboard.getProductsInOrder(orderId)
 
+    if (!result) {
+      res.status(404).json(`No order with id ${orderId}`)
+    }
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
 
+// Checkout a user order
+export const checkoutOrder = async (_req: Request, res: Response) => {
+  try {
+    const orderId = _req.params.orderId as unknown as number
+    const userId = _req.params.userId as unknown as number
+
+    const orderExist = await orderStore.getSingleOrder(orderId)
+
+    if (orderExist) {
+      if (orderExist.status === 'ACTIVE') {
+        orderExist.status = 'COMPLETE'
+        const result = await dashboard.checkOutOrder(
+          orderId,
+          userId,
+          orderExist.status
+        )
+
+        res.status(200).json(result)
+      } else {
+        res.status(400).json(`Order ${orderId} is already checkouted out!!`)
+      }
+    } else {
+      res.status(404).json(`The order ${orderId} doesnot exist!!`)
+    }
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,121 +1,114 @@
-import { Order, OrderModel } from "../models/orders.model"
-import { Request, Response } from "express"
-
+import { Order, OrderModel } from '../models/orders.model'
+import { Request, Response } from 'express'
 
 const orderStore = new OrderModel()
 
 const getOrders = async (_req: Request, res: Response) => {
-    try {
-        const orders = await orderStore.getAllOrders()
+  try {
+    const orders = await orderStore.getAllOrders()
 
-        res.status(200).json(orders)
-        
-    } catch (error) {
-        res.status(400).json(error)
-    }
+    res.status(200).json(orders)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 }
-
 
 const getOrder = async (_req: Request, res: Response) => {
-    try {
-        const order = await orderStore.getSingleOrder(_req.params.id as unknown as number)
+  try {
+    const order = await orderStore.getSingleOrder(
+      _req.params.id as unknown as number
+    )
 
-        if(!order){
-            res.status(404).json(`The order doesnt exist!`)
-        }else{
-            res.status(200).json(order)
-        }
-        
-    } catch (error) {
-        res.status(400).json(error)
+    if (!order) {
+      res.status(404).json(`The order doesnt exist!`)
+    } else {
+      res.status(200).json(order)
     }
+  } catch (error) {
+    res.status(400).json(error)
+  }
 }
 
-
 const createOrder = async (_req: Request, res: Response) => {
-    try {
-
-
-        const orderObject = {
-            user_id: _req.body.user_id,
-            status: _req.body.status
-        }
-
-        const orders = await orderStore.create(orderObject)
-
-        res.status(200).json(orders)
-        
-    } catch (error) {
-        res.status(400).json(error)
+  try {
+    const orderObject = {
+      user_id: _req.body.user_id,
+      status: _req.body.status,
     }
+
+    const orders = await orderStore.create(orderObject)
+
+    res.status(200).json(orders)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 }
 
 const createProductOrder = async (_req: Request, res: Response) => {
-    try {
-        const orderId = _req.params.orderId as unknown as number
-        const productId = _req.params.productId as unknown as number
-        const qty = _req.body.quantity
-        
-        const orderProductObject = {
-            quantity: qty,
-            order_id: orderId,
-            product_id: productId
-        }
+  try {
+    const orderId = _req.params.orderId as unknown as number
+    const productId = _req.params.productId as unknown as number
+    const qty = _req.body.quantity
 
-        const result = await orderStore.createOrderWithProducts(orderProductObject)
-        res.status(200).json(result)
-        
-    } catch (error) {
-        res.status(400).json(error)
+    const orderProductObject = {
+      quantity: qty,
+      order_id: orderId,
+      product_id: productId,
     }
-}
 
+    const result = await orderStore.createOrderWithProducts(orderProductObject)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
 
 const updateOrder = async (_req: Request, res: Response) => {
-    try {
+  try {
+    const order = await orderStore.getSingleOrder(
+      _req.params.id as unknown as number
+    )
 
-        const order = await orderStore.getSingleOrder(_req.params.id as unknown as number)
-
-        if(!order){
-            res.status(404).json(`The order doesnt exist!`)
-        }else{
-            const orderObject = {
-                id: order.id,
-                user_id: _req.body.user_id,
-                status: _req.body.status
-            }
-            const result = await orderStore.updateSingleOrder(orderObject)
-            res.status(200).json(result)
-        }
-        
-    } catch (error) {
-        res.status(400).json(error)
+    if (!order) {
+      res.status(404).json(`The order doesnt exist!`)
+    } else {
+      const orderObject = {
+        id: order.id,
+        user_id: _req.body.user_id,
+        status: _req.body.status,
+      }
+      const result = await orderStore.updateSingleOrder(orderObject)
+      res.status(200).json(result)
     }
+  } catch (error) {
+    res.status(400).json(error)
+  }
 }
 
-
 const deleteOrder = async (_req: Request, res: Response) => {
-    try {
+  try {
+    const order = await orderStore.getSingleOrder(
+      _req.params.id as unknown as number
+    )
 
-        const order = await orderStore.getSingleOrder(_req.params.id as unknown as number)
-
-        if(!order){
-            res.status(404).json(`The order doesnt exist!`)
-        }else{
-            const result = await orderStore.deleteSingleOrder(_req.params.id as unknown as number)
-            res.status(200).json(`Order with id ${_req.params.id} has been deleted!`)
-        }
-        
-    } catch (error) {
-        res.status(400).json(error)
+    if (!order) {
+      res.status(404).json(`The order doesnt exist!`)
+    } else {
+      const result = await orderStore.deleteSingleOrder(
+        _req.params.id as unknown as number
+      )
+      res.status(200).json(`Order with id ${_req.params.id} has been deleted!`)
     }
+  } catch (error) {
+    res.status(400).json(error)
+  }
 }
 
 export {
-    createOrder,
-    createProductOrder,
-    getOrders,
-    getOrder,
-    updateOrder,
-    deleteOrder
+  createOrder,
+  createProductOrder,
+  getOrders,
+  getOrder,
+  updateOrder,
+  deleteOrder,
 }
