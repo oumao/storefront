3bc957ae69373f4a9c5f0eb6ae48abda8dd587c8
oumao <evansotis2015@gmail.com>
2023-01-19 23:46:26 +0300

    Test Refactor

--- src/tests/controllers/dashboardSpec.ts
+++ src/tests/controllers/dashboardSpec.ts
@@ -1,6 +1,7 @@
 import supertest from 'supertest'
 import app from '../../server'
 import dotenv from 'dotenv'
+import jwt from  'jsonwebtoken'
 import { ProductModel } from '../../models/products.model'
 import { UserModel } from '../../models/users.model'
 import { OrderModel } from '../../models/orders.model'
@@ -10,53 +11,38 @@ import { createToken } from '../utilities/createToken'
 dotenv.config()
 
 const request = supertest(app)
-const productStore = new ProductModel()
-const userStore = new UserModel() 
-const orderStore = new OrderModel()
+// const productStore = new ProductModel()
+// const userStore = new UserModel() 
+// const orderStore = new OrderModel()
 
 
 describe("Dashboard Endpoints Suite", () =>{
-    let token: string
+    let token: any
     
     describe('[GET] api/dashboard/user-order', () => {
         
 
-        // beforeAll(async () => {
-        //     // await productStore.create({
-        //     //     name: 'Sonytech',
-        //     //     price: 200.0,
-        //     //   })
-          
-        //     //   await userStore.createUser({
-        //     //     username: 'test_oumao',
-        //     //     firstName: 'John',
-        //     //     lastName: 'Doe',
-        //     //     password_digest: 'password123',
-        //     //   })
-      
-        //     //   await orderStore.create({
-        //     //       user_id: 1,
-        //     //       status: "ACTIVE"
-        //     //   })
-      
-        //     //   await orderStore.createOrderWithProducts({
-        //     //       quantity: 3,
-        //     //       order_id: 1,
-        //     //       product_id: 1
-        //     //   })
+        beforeAll(async () => {
+            const user = await request
+            .post("/api/auth/sign-in")
+            .send({
+                username: "testUser",
+                password_digest:  "password"
+            })
 
+            token = user.body
               
-        // })
+        })
         
         it("Should get specific user with a specific order", async () => {
 
-            
-             const response = await request
-                 .post('/api/dashboard/user-order')
-                 .set('Authorization', `Bearer ${token}`)
-                 .query({ user_id: 100})
-                 .expect(200)
+        
+            const response = await request
+                .post('/api/dashboard/user-order')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(200)
                  
+            console.log(response.body)
             expect(response.body).toEqual({
                 firstName: "John",
                 lastName: "Doe",
@@ -69,9 +55,8 @@ describe("Dashboard Endpoints Suite", () =>{
 
         it("Should return 404 incase user not found", async () => {
             const response = await request
-                    .get('/api/dashboard/user-order')
+                    .get('/api/dashboard/user-order/1')
                     .set('Authorization', `Bearer ${token}`)
-                    .query({ user_id: 100})
 
             expect(response.status).toEqual(404)
         })
@@ -80,10 +65,9 @@ describe("Dashboard Endpoints Suite", () =>{
         it("should return 400 if an error occurs", async () => {
             // Simulate an error by passing an invalid user_id
             const response = await request
-                .get('/dashboard/user-order')
+                .get('/dashboard/user-order/inval')
                 .set('Authorization', `Bearer ${token}`)
-                .query({user_id: 'invalid_id'});
-        
+
             expect(response.status).toEqual(400);
             expect(response.body).toEqual({
                 message: 'Invalid user_id'
