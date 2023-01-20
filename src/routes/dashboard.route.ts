c173ef370de1b997431bab1fb5d3fff7bf8ef654
oumao <evansotis2015@gmail.com>
2023-01-20 23:51:15 +0300

    Updated the query param of user-order

--- src/routes/dashboard.route.ts
+++ src/routes/dashboard.route.ts
@@ -9,7 +9,7 @@ import { verifyToken } from '../middlewares/verifyToken.middlewares'
 
 const dashboardRoutes = express.Router()
 
-dashboardRoutes.route('/dashboard/user-order').get(verifyToken, getUserOrder)
+dashboardRoutes.route('/dashboard/user-order/:userId').get(verifyToken, getUserOrder)
 dashboardRoutes.route('/dashboard/users-orders').get(verifyToken, getUsersOrders)
 dashboardRoutes.route('/cart/:orderId').get(verifyToken, getProductsOrder)
 dashboardRoutes.route('/users/:userId/checkout/:orderId').put(verifyToken, checkoutOrder)
