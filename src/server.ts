d628fb7c06bf9809b9b1123a783219aed25ad734
oumao <evansotis2015@gmail.com>
2023-01-21 22:57:17 +0300

    Added CORS

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,24 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
 import apiRoutes from './routes/index.route'
+import cors from 'cors'
 
 const app: express.Application = express()
 const address = '0.0.0.0:3000' as string
+
+const corsOptions = {
+  origin: '*',
+  optionsSucccessStatus: 200
+}
+
 
 app.use(bodyParser.json())
 
 // Routes
 app.use('/api', apiRoutes)
+
+// Middlewares
+app.use(cors(corsOptions))
 
 app.listen(3000, function () {
   console.log(`starting app on: ${address}`)
